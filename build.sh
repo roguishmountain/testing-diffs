f93269d4d77fe08d29b28f84c234a9e9110e5f3f
Sam Schwarz <roguishmountain@gmail.com>
2017-10-10 10:44:16 -0400

    artifact

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 touch file1 file2 file3
 
-echo "##vso[task.addattachment type=text;name=myattachmentname;]file1"
+# echo "##vso[task.addattachment type=text;name=myattachmentname;]file1"
+
+echo "##vso[artifact.upload containerfolder=testresult;artifactname=uploadedresult;]file1"
